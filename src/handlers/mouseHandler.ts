e6c046fa33d9c412a03e46bca207a49f654a0106
Danny <justforgithub.poarch@gmail.com>
2026-03-30 08:45:19 -0400

    Update mouseHandler.ts

--- src/handlers/mouseHandler.ts
+++ src/handlers/mouseHandler.ts
@@ -13,25 +13,30 @@ export class MouseHandler {
   init() {
     this.canvas.addEventListener("mousemove", this.handleMouseMove.bind(this));
     this.canvas.addEventListener("mousedown", this.handleMouseDown.bind(this));
-
     window.addEventListener("mouseup", this.handleMouseUp.bind(this));
 
     this.canvas.addEventListener(
       "touchstart",
-      this.handleTouchStart.bind(this),
+      (e) => {
+        this.handleTouchStart(e);
+      },
       { passive: false },
     );
+
     this.canvas.addEventListener("touchmove", this.handleTouchMove.bind(this), {
       passive: false,
     });
     window.addEventListener("touchend", this.handleTouchEnd.bind(this));
-    window.addEventListener("touchcancel", this.handleTouchEnd.bind(this));
   }
 
   private updatePosition(clientX: number, clientY: number) {
     const rect = this.canvas.getBoundingClientRect();
-    this.x = clientX - rect.left;
-    this.y = clientY - rect.top;
+
+    const scaleX = this.canvas.width / rect.width;
+    const scaleY = this.canvas.height / rect.height;
+
+    this.x = (clientX - rect.left) * scaleX;
+    this.y = (clientY - rect.top) * scaleY;
   }
 
   handleMouseMove(event: MouseEvent) {
